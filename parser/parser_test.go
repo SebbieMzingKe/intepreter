1f79686a60cfc9f1b05f472c3a3e9cd9a15f5e9c
SebbieMzingKe <sebbievilar2@gmail.com>
2026-02-28 13:40:21 +0300

    chore(parser): giving errors for each erroneous statement it encounters.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -18,6 +18,8 @@ func TestLetStatement(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -43,6 +45,23 @@ func TestLetStatement(t *testing.T) {
 		}
 	}
 }
+
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
+}
+
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
